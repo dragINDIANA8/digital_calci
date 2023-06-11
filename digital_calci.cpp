f27e8261b8695adf34ce36dd81416161482c22f1
LetMeCodeCInCPP <135963490+LetMeCodeCInCPP@users.noreply.github.com>
2023-06-11 15:47:58 +0000

    Update digital_calci.cpp

--- digital_calci.cpp
+++ digital_calci.cpp
@@ -1,81 +1,37 @@
 #include <iostream>
-using namespace std;
-void addition();
-void subtraction();
-void multiplication();
-void division();
-
 int main()
 {
-    cout << "This is the Digital Calculator developed by SHUBHRANSHU SHEKHAR DAS!!\n"
-         << endl;
-    cout << "*******Press 0 to quit the program*******\n"
-         << endl;
-    cout << "Enter 1 for Addition \n";
-    cout << "Enter 2 for Subtraction \n";
-    cout << "Enter 3 for Multiplication \n";
-    cout << "Enter 4 for Division \n\n";
-    int choice;
-    while (1)
+    char op {};
+    int a {}, b {}, result {};
+    std::cout << "This is the Digital Calculator developed by SHUBHRANSHU SHEKHAR DAS!!" << std::endl;
+    std::cout << "*******Press 0 to quit the program*******" << std::endl;
+    std::cout << "What is your fist digit?: " << std::endl;
+    std::cin >> a;
+    std::cout << "What is your operator (+, -, *, /): " << std::endl;
+    std::cin >> op;
+    std::cout << "What is your second digit?: " << std::endl;
+    std::cin >> b;
+    switch (op)
     {
-        cout << "ENTER YOUR CHOICE: ";
-        cin >> choice;
-
-        switch (choice)
-        {
-        case 1:
-            addition();
-            break;
-        case 2:
-            subtraction();
-            break;
-        case 3:
-            multiplication();
-            break;
-        case 4:
-            division();
-            break;
-        case 0:
-            exit(0);
-            break;
-        default:
-            cout << "\n******Wrong choice, Enter valid choice******\n\n";
-            break;
-        }
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+    case '0':
+        exit(0);
+        break;
+    default:
+        std::cout << "\n******Wrong op, Enter valid op******\n\n";
+        break;
     }
-
+    std::cout << "Result = " << result << std::endl;
     return 0;
-}
-
-void addition()
-{
-    cout << "Enter the numbers you want to add: ";
-    int a, b;
-    cin >> a >> b;
-    cout << "The addition of " << a << " and " << b << " is " << a + b << endl
-         << endl;
-}
-void subtraction()
-{
-    cout << "Enter the numbers you want to subtract: ";
-    int a, b;
-    cin >> a >> b;
-    cout << "The subtraction of " << a << " and " << b << " is " << a - b << endl
-         << endl;
-}
-void multiplication()
-{
-    cout << "Enter the numbers you want to multipliation: ";
-    int a, b;
-    cin >> a >> b;
-    cout << "The multiplication of " << a << " and " << b << " is " << a * b << endl
-         << endl;
-}
-void division()
-{
-    cout << "Enter the numbers you want to divide: ";
-    float a, b;
-    cin >> a >> b;
-    cout << "The division of " << a << " and " << b << " is " << a / b << endl
-         << endl;
 }
